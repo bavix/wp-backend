da1838bc297b4f13555854fb854d3b93c99c88ad
Babichev Maxim <info@babichev.net>
2018-06-19 20:28:07 +0300

    add style

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\User\StoreRequest;
 use App\Models\User\Contact;
 use App\Models\User\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
---
+++ app/Models/Wheel/Style.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Wheel;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Style extends Model
+{
+
+}
