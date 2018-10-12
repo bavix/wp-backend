e729db657a93157488edcdd357cf1bfd4b23fa20
Babichev Maxim <babichev@mail.tvil.ru>
2018-10-12 12:21:25 +0300

    the distribution of roles by user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,10 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         $this->call(UsersTableSeeder::class);
-         $this->call(WheelsTableSeeder::class);
-         $this->call(LikesTableSeeder::class);
-         $this->call(FollowersTableSeeder::class);
-         $this->call(RolesTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(WheelsTableSeeder::class);
+        $this->call(LikesTableSeeder::class);
+        $this->call(FollowersTableSeeder::class);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,6 +13,26 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         factory(\App\Models\User::class, 100)->create();
+
+        $output = $this->command->getOutput();
+        $progressBar = $output->createProgressBar(
+            \App\Models\User::query()->count()
+        );
+
+        \App\Models\User::query()->each(function (\App\Models\User $user) use ($progressBar) {
+
+            /**
+             * @var $role \Yajra\Acl\Models\Role
+             */
+            $role = \Yajra\Acl\Models\Role::query()
+                ->inRandomOrder()
+                ->first();
+
+            $user->attachRole($role);
+            $progressBar->advance();
+        });
+
+        $output->newLine();
     }
 
 }
