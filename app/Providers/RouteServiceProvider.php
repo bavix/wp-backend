30c557b127bd160ab6f95da70e06f2bdd95f6061
Babichev Maxim <info@babichev.net>
2019-04-08 21:21:02 +0300

    recovery ['api']

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -48,6 +48,7 @@ class RouteServiceProvider extends ServiceProvider
     protected function mapApiRoutes()
     {
         Route::prefix('api')
+            ->middleware(['api'])
 //            ->middleware(['api', 'client_credentials'])
             ->namespace('App\Http\Api')
             ->name('api.')
