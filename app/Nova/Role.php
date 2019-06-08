c47398561cdff94e85cad3b74d27f9519daf037b
Babichev Maxim <babichev@mail.tvil.ru>
2019-06-08 11:42:11 +0300

    add nova models

---
+++ app/Nova/Role.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace App\Nova;
+
+use Laravel\Nova\Fields\BelongsToMany;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+use Laravel\Nova\Fields\Textarea;
+
+class Role extends Resource
+{
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \App\Models\Role::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * @var string
+     */
+    public static $group = 'User management';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'name', 'description',
+    ];
+
+    /**
+     * @return string
+     */
+    public function subtitle(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            ID::make()->sortable(),
+
+            BelongsToMany::make('Users'),
+            BelongsToMany::make('Permissions'),
+
+            Text::make('Name')
+                ->rules('required'),
+
+            Text::make('Slug')
+                ->rules('required'),
+
+            Textarea::make('Description')
+                ->rules('required'),
+
+            Boolean::make('System'),
+
+            DateTime::make('Created At')
+                ->hideFromIndex()
+                ->hideWhenCreating()
+                ->hideWhenUpdating()
+                ->readonly(true),
+
+            DateTime::make('Updated At')
+                ->hideWhenCreating()
+                ->hideWhenUpdating()
+                ->readonly(true),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request): array
+    {
+        return [];
+    }
+
+}
