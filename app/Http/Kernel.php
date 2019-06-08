4c8a6059af7eb4a03baa0bb4a0c97ca783120098
Babichev Maxim <babichev@mail.tvil.ru>
2019-06-08 16:36:01 +0300

    update cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -39,7 +39,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            \Barryvdh\Cors\HandleCors::class,
+            \Spatie\Cors\Cors::class,
             'guard:api',
             'throttle:120,1',
             'bindings',
