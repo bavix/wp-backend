f9dff57fc51935b22bc30724a6e491ce8c9491f0
Babichev Maxim <babichev@mail.tvil.ru>
2019-06-08 17:24:58 +0300

    add passport model's to nova

---
+++ app/Nova/PassportToken.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Nova;
+
+use Carbon\Carbon;
+use Laravel\Nova\Fields\BelongsTo;
+use Laravel\Nova\Fields\Boolean;
+use Laravel\Nova\Fields\DateTime;
+use Laravel\Nova\Fields\HasMany;
+use Laravel\Nova\Fields\ID;
+use Illuminate\Http\Request;
+use Laravel\Nova\Fields\Text;
+
+class PassportToken extends Resource
+{
+
+    /**
+     * The model the resource corresponds to.
+     *
+     * @var string
+     */
+    public static $model = \Laravel\Passport\Token::class;
+
+    /**
+     * The single value that should be used to represent the resource when being displayed.
+     *
+     * @var string
+     */
+    public static $title = 'name';
+
+    /**
+     * @var string
+     */
+    public static $group = 'User management';
+
+    /**
+     * The columns that should be searched.
+     *
+     * @var array
+     */
+    public static $search = [
+        'name',
+    ];
+
+    /**
+     * Get the fields displayed by the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function fields(Request $request): array
+    {
+        return [
+            ID::make()->sortable(),
+
+            BelongsTo::make('Passport Client', 'Client'),
+            BelongsTo::make('User'),
+
+            Text::make('Name')
+                ->rules('required'),
+
+            Text::make('Scopes')
+                ->hideFromIndex()
+                ->rules('required'),
+
+            Boolean::make('Revoked'),
+
+            DateTime::make('Expires At'),
+
+            DateTime::make('Created At')
+                ->resolveUsing(function ($value) {
+                    return Carbon::make($value);
+                })
+                ->hideFromIndex()
+                ->hideWhenCreating()
+                ->hideWhenUpdating()
+                ->readonly(true),
+
+            DateTime::make('Updated At')
+                ->resolveUsing(function ($value) {
+                    return Carbon::make($value);
+                })
+                ->hideWhenCreating()
+                ->hideWhenUpdating()
+                ->readonly(true),
+        ];
+    }
+
+    /**
+     * Get the cards available for the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function cards(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the filters available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function filters(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the lenses available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function lenses(Request $request): array
+    {
+        return [];
+    }
+
+    /**
+     * Get the actions available for the resource.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function actions(Request $request): array
+    {
+        return [];
+    }
+
+}
